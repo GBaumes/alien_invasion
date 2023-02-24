74814208710fd3b1931eff9d0fc49a4edb53e939
GBaumes <baumesgreg@gmail.com>
2023-02-24 17:03:00 -0500

    Fixed window instantly closing

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,5 @@
+# Import the sys and pygame modules.
 import sys
-
 import pygame
 
 class AlienInvasion:
@@ -7,20 +7,26 @@ class AlienInvasion:
 
     def __init__(self):
         """Initialize the game, and create game resources."""
+        # Initializes the background settings that Pygame needs to work properly.
         pygame.init()
-
+        
+        # Create a display window (The object assigned to self.screen is called a surface)
+        # A surface in Pygame is a part of the screen where a game element can be displayed.
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
-
+    
+    # The game is controlled by the run_game() method.
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             # Watch the keyboard and mouse events.
+            # Event loop to listen for events and perform appropriate tasks depending on the kind of event
             for event in pygame.event.get():
-                if event.type == pygame.QUIT():
+                if event.type == pygame.QUIT:
                     sys.exit()
 
             # Make the most recently drawn screen visible.
+            # Tells Pygame to make the most recently drawn screen visible.
             pygame.display.flip()
 
 if __name__ == '__main__':
