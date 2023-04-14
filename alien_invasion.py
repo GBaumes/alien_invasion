11363cc88fcbce5bfb615dfc18f1e685bd7aee27
GBaumes <baumesgreg@gmail.com>
2023-04-14 14:14:38 -0400

    Display high score

--- alien_invasion.py
+++ alien_invasion.py
@@ -159,6 +159,7 @@ class AlienInvasion:
                 self.stats.score += self.settings.alien_points * len(aliens)
             #self.stats.score += self.settings.alien_points
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet
